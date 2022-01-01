4ddcf89fc88dd86aa64685df9dbdfefeb2469249
renby <renfyg@yonyou.com>
2022-01-01 22:56:15 +0800

    init

---
+++ springcloud-all/src/test/java/com/teabag/springcloudall/SpringcloudAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teabag.springcloudall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
